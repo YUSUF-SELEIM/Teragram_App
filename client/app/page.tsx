275a9583bf4555e8a5f34a65ef88e77203383ab2
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-31 02:31:22 +0300

    added auth forms and image

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+import { useState } from "react";
+
+import { ThemeSwitch } from "@/components/theme-switch";
+import SignUpForm from "@/components/SignUpForm";
+import LoginForm from "@/components/LoginForm";
+
 export default function Home() {
+  const [isSignUp, setIsSignUp] = useState(true);
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="justify-center inline-block max-w-lg text-center">
-        <br />
-        <h1>websites regardless of your design experience.</h1>
-        Beautiful, fast and modern React UI library.
+    <section className="flex h-screen bg-white dark:bg-black ">
+      <div className="hidden w-2/3 shadow-2xl Teragram md:block " />
+      <div className="flex flex-col items-center justify-center w-full border border-violet-700 md:w-1/2">
+        <div className="absolute top-0 right-0 p-4">
+          <ThemeSwitch />
+        </div>
+        <h1 className="my-5 text-4xl font-bold text-center text-black transition-transform duration-200 transform dark:text-white hover:scale-105">
+          Tera<span className="text-violet-700">Gram</span>
+        </h1>
+        <div className="flex flex-col w-full max-w-md p-4 space-y-6 dark:bg-black">
+          {isSignUp ? (
+            <SignUpForm setIsSignUp={setIsSignUp} />
+          ) : (
+            <LoginForm setIsSignUp={setIsSignUp} />
+          )}
+        </div>
       </div>
     </section>
   );
