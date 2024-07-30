1450ae2e78d496ab79260fd6516d2ff7ab6319bd
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-31 02:29:26 +0300

    removed default styling

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -34,11 +34,8 @@ export default function RootLayout({
         className={clsx("min-h-screen bg-background font-sans antialiased")}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <main className="container flex-grow px-6 pt-16 mx-auto max-w-7xl">
-              {children}
-            </main>
-            <footer className="flex items-center justify-center w-full py-3" />
+          <div>
+            <main>{children}</main>
           </div>
         </Providers>
       </body>
