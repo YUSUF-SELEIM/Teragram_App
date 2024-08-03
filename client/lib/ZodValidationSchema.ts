8b0514de441ce8b8c1b56bcf8a5309c4517fc72f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-04 02:24:58 +0300

    Added Zod validation schema for login and signup forms

---
+++ client/lib/ZodValidationSchema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const signUpSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm password must be at least 6 characters long")
+      .nonempty("Confirm password is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
