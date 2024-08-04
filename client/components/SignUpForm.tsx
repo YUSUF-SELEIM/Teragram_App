c2ee3375ee9fbc876b65fc7308fab14031e427ea
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-04 23:51:36 +0300

    Integrated REGISTER MUTATION with SignupForm
    Added input fields of name and confirm password and their validations
    
    Added a Loading state to sign up button

--- client/components/SignUpForm.tsx
+++ client/components/SignUpForm.tsx
@@ -1,13 +1,59 @@
-import { Button, Input } from "@nextui-org/react";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { AiFillMail, AiFillLock } from "react-icons/ai";
+import { Button, Input, Spinner } from "@nextui-org/react";
+import { z } from "zod";
+
+import { signUpSchema } from "../lib/ZodValidationSchema";
+
+import { useAuth } from "@/hooks/useAuth";
+
+type SignUpFormData = z.infer<typeof signUpSchema>;
 
 function SignUpForm({
   setIsSignUp,
 }: {
   setIsSignUp: (value: boolean) => void;
 }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  const { register: registerUser } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit: SubmitHandler<SignUpFormData> = async (data) => {
+    try {
+      setIsLoading(true);
+      console.log("Form data:", data);
+
+      // Call the register mutation
+      const response = await registerUser({
+        variables: {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+          confirmPassword: data.confirmPassword,
+        },
+      });
+
+      console.log("Registration successful:", response);
+
+      // Redirect to login form
+      setIsSignUp(false);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error registering:", error);
+    }
+  };
+
   return (
-    <form className="space-y-4">
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
       <h2 className="text-2xl font-bold text-center text-black transition-transform duration-200 transform dark:text-white hover:scale-105">
         Sign Up
       </h2>
@@ -16,8 +62,13 @@ function SignUpForm({
           className="dark:bg-black dark:border-white dark:text-white"
           label="Name"
           placeholder="Enter your name"
+          {...register("name")}
+          isInvalid={!!errors.name}
           variant="bordered"
         />
+        {errors.name && (
+          <p className="text-xs text-red-500">{errors.name.message}</p>
+        )}
       </div>
       <div>
         <Input
@@ -27,8 +78,13 @@ function SignUpForm({
           }
           label="Email"
           placeholder="Enter your email"
+          {...register("email")}
+          isInvalid={!!errors.email}
           variant="bordered"
         />
+        {errors.email && (
+          <p className="text-xs text-red-500">{errors.email.message}</p>
+        )}
       </div>
       <div>
         <Input
@@ -39,8 +95,13 @@ function SignUpForm({
           label="Password"
           placeholder="Enter your password"
           type="password"
+          {...register("password")}
+          isInvalid={!!errors.password}
           variant="bordered"
         />
+        {errors.password && (
+          <p className="text-xs text-red-500">{errors.password.message}</p>
+        )}
       </div>
       <div>
         <Input
@@ -51,11 +112,22 @@ function SignUpForm({
           label="Confirm Password"
           placeholder="Confirm your password"
           type="password"
+          {...register("confirmPassword")}
+          isInvalid={!!errors.confirmPassword}
           variant="bordered"
         />
+        {errors.confirmPassword && (
+          <p className="text-xs text-red-500">
+            {errors.confirmPassword.message}
+          </p>
+        )}
       </div>
-      <Button className="w-full text-white bg-violet-700" variant="shadow">
-        Sign Up
+      <Button
+        className="w-full text-white bg-violet-700"
+        type="submit"
+        variant="shadow"
+      >
+        {isLoading ? <Spinner color="white" size="sm" /> : "Sign Up"}
       </Button>
       <p className="text-sm text-center text-gray-600 dark:text-gray-300">
         Already have an account?{" "}
