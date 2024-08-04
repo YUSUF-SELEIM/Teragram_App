040433a830818ec88e377f927657c0185f3c80fb
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-04 03:54:12 +0300

    Added Login Form that includes Form Validation graphql LOGIN mutation a set-cookie API route for setting cookie by server side

--- client/components/LoginForm.tsx
+++ client/components/LoginForm.tsx
@@ -1,9 +1,79 @@
-import { Button, Input } from "@nextui-org/react";
+"use client";
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { AiFillMail, AiFillLock } from "react-icons/ai";
+import { Button, Input } from "@nextui-org/react";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+
+import { loginSchema } from "../lib/ZodValidationSchema";
+
+import { useAuth } from "@/hooks/useAuth";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 function LoginForm({ setIsSignUp }: { setIsSignUp: (value: boolean) => void }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormValues>({
+    resolver: zodResolver(loginSchema),
+  });
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    try {
+      const response = await login({
+        variables: {
+          email: data.email,
+          password: data.password,
+        },
+      });
+
+      const { token } = response.data.login;
+
+      // Decode the token to get user info
+      const decoded: any = jwtDecode(token);
+      const userId = decoded.id;
+
+      // Make a request to Express server to set the HttpOnly cookie
+      const cookieResponse = await fetch(
+        "http://localhost:5000/api/set-cookie",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({ token }),
+        },
+      );
+
+      if (cookieResponse.ok) {
+        // Handle successful cookie setting, such as redirecting the user
+        console.log("Cookie set successfully");
+      } else {
+        console.error("Failed to set cookie");
+      }
+      router.push(`/chats/${userId}`);
+    } catch (error: any) {
+      // Handling server-side errors
+      const serverError = error.message;
+
+      // Set server-side error message
+      setError("password", { message: serverError });
+    }
+  };
+
   return (
-    <form className="space-y-4">
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
       <h2 className="text-2xl font-bold text-center text-black transition-transform duration-200 transform dark:text-white hover:scale-105">
         Log In
       </h2>
@@ -16,7 +86,11 @@ function LoginForm({ setIsSignUp }: { setIsSignUp: (value: boolean) => void }) {
           label="Email"
           placeholder="Enter your email"
           variant="bordered"
+          {...register("email")}
         />
+        {errors.email && (
+          <p className="text-sm text-red-500">{errors.email.message}</p>
+        )}
       </div>
       <div>
         <Input
@@ -28,11 +102,18 @@ function LoginForm({ setIsSignUp }: { setIsSignUp: (value: boolean) => void }) {
           placeholder="Enter your password"
           type="password"
           variant="bordered"
+          {...register("password")}
         />
+        {errors.password && (
+          <p className="text-sm text-red-500">{errors.password.message}</p>
+        )}
       </div>
-
-      <Button className="w-full text-white bg-violet-700" variant="shadow">
-        Sign In
+      <Button
+        className="w-full text-white bg-violet-700"
+        type="submit"
+        variant="shadow"
+      >
+        Log In
       </Button>
       <p className="text-sm text-center text-gray-600 dark:text-gray-300">
         Don&apos;t have an account?{" "}
