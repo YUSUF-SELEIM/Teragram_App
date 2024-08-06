5bd705ffc5885007fc2f653afde5b082b47e53fe
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-06 08:17:29 +0300

    Changed theme switch Icons to ones from react-icons/bi

--- client/components/theme-switch.tsx
+++ client/components/theme-switch.tsx
@@ -6,8 +6,7 @@ import { SwitchProps, useSwitch } from "@nextui-org/switch";
 import { useTheme } from "next-themes";
 import { useIsSSR } from "@react-aria/ssr";
 import clsx from "clsx";
-
-import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
+import { BiSun, BiMoon } from "react-icons/bi";
 
 export interface ThemeSwitchProps {
   className?: string;
@@ -61,7 +60,7 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
               "rounded-lg",
               "flex items-center justify-center",
               "group-data-[selected=true]:bg-transparent",
-              "!text-default-500",
+              "!text-default-foreground",
               "pt-px",
               "px-0",
               "mx-0",
@@ -71,9 +70,9 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
         })}
       >
         {!isSelected || isSSR ? (
-          <SunFilledIcon size={22} />
+          <BiSun className="text-2xl transform hover:scale-125 hover:text-violet-400" />
         ) : (
-          <MoonFilledIcon size={22} />
+          <BiMoon className="text-2xl transform hover:scale-125 hover:text-violet-400" />
         )}
       </div>
     </Component>
