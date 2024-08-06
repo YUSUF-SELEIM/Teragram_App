c776cf82fe91c2e09a966996f2088471b32feb7a
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-06 08:21:36 +0300

    Edited dashboard icons and removed extra icons , made it more simple

--- client/components/dashboard.tsx
+++ client/components/dashboard.tsx
@@ -1,62 +1,39 @@
 import React from "react";
-import {
-  AiOutlineUser,
-  AiOutlineMessage,
-  AiOutlineTeam,
-  AiOutlineSetting,
-  AiOutlineLogout,
-} from "react-icons/ai";
-import Image from "next/image";
+import { BiUserCircle, BiLogOutCircle } from "react-icons/bi";
 import nookies from "nookies";
 import { useRouter } from "next/navigation";
 
-const Dashboard = () => {
+import { ThemeSwitch } from "./theme-switch";
+
+const Dashboard = ({ toggleUserInfo }: { toggleUserInfo: () => void }) => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-between p-4 shadow-xl dark:text-white">
-      <Image
-        alt={"logo"}
-        className="transition-transform transform duration-250 hover:-rotate-12"
-        height={50}
-        src="/logo.png"
-        width={50}
-      />
+    <div className="relative flex flex-col items-center justify-between h-full p-4 bg-neutral-200 dark:bg-neutral-950 py-7 dark:text-white">
       <nav>
         <ul className="flex flex-col items-center h-full space-y-8">
           <li>
-            <button onClick={() => {}}>
-              <AiOutlineUser className="text-2xl transform hover:scale-125 hover:text-violet-400" />
+            <button onClick={toggleUserInfo}>
+              <BiUserCircle className="text-2xl transform hover:scale-125 hover:text-violet-400" />
             </button>
           </li>
+
           <li>
-            <button onClick={() => {}}>
-              <AiOutlineMessage className="text-2xl transform hover:scale-125 hover:text-violet-400" />
-            </button>
+            <ThemeSwitch />
           </li>
           <li>
-            <button onClick={() => {}}>
-              <AiOutlineTeam className="text-2xl transform hover:scale-125 hover:text-violet-400" />
-            </button>
-          </li>
-          <li>
-            <button onClick={() => {}}>
-              <AiOutlineSetting className="text-2xl transform hover:scale-125 hover:text-violet-400" />
+            <button
+              onClick={() => {
+                // Destroy token cookie
+                nookies.destroy(null, "token", { path: "/" });
+                router.push("/");
+              }}
+            >
+              <BiLogOutCircle className="text-2xl transform hover:scale-125 hover:text-violet-400" />
             </button>
           </li>
         </ul>
       </nav>
-      <div>
-        <button
-          onClick={() => {
-            // Destroy token cookie
-            nookies.destroy(null, "token", { path: "/" });
-            router.push("/");
-          }}
-        >
-          <AiOutlineLogout className="text-2xl transform rotate-180 hover:scale-125 hover:text-violet-400" />
-        </button>
-      </div>
     </div>
   );
 };
