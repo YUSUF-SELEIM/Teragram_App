a6551f21170c6f0e7b4632f7bd38553e23e2c742
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-07 07:23:39 +0300

    Conditionally rendering  Users component and defined it's visibility state

--- client/components/sidebar.tsx
+++ client/components/sidebar.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { FaPen } from "react-icons/fa";
 
 import Search from "./search";
 import UserInfo from "./userInfo";
 import PopOver from "./popover";
+import Users from "./users";
 
 function Sidebar({
   id,
@@ -15,6 +16,8 @@ function Sidebar({
   toggleDashboard: () => void;
   isUserInfoVisible: boolean;
 }) {
+  const [isUsersVisible, setIsUsersVisible] = useState<boolean>(false);
+
   return (
     <div className="relative flex flex-col items-center justify-between w-full p-4 md:w-[45%] bg-neutral-100 dark:bg-neutral-900 dark:text-white">
       <div className="flex items-center w-full space-x-2">
@@ -29,8 +32,9 @@ function Sidebar({
         <Search />
       </div>
       {isUserInfoVisible && <UserInfo />}
+      {isUsersVisible && <Users setIsUsersVisible={setIsUsersVisible} />}
       <div className="flex items-center justify-end w-full">
-        <PopOver>
+        <PopOver setIsUsersVisible={setIsUsersVisible}>
           <button className="p-5 text-white rounded-full shadow-2xl bg-violet-700">
             <FaPen className="text-lg transition-transform transform text-md duration-250 hover:-rotate-12" />
           </button>
