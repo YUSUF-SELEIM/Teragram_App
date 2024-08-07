dcc759d3fac877778f4fdfc12c364d49f43e46d9
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-07 07:25:36 +0300

    Linked "New Chat " button to toggleUsers function that shows Users component

--- client/components/popover.tsx
+++ client/components/popover.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
 import { BiSolidUserPlus, BiSolidGroup } from "react-icons/bi";
 
-export default function PopOver({ children }: { children: React.ReactNode }) {
+export default function PopOver({
+  children,
+  setIsUsersVisible,
+}: {
+  children: React.ReactNode;
+  setIsUsersVisible: (value: boolean) => void;
+}) {
   const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleUsers = () => {
+    setIsOpen(false);
+    setIsUsersVisible(true);
+  };
 
   return (
     <div className="flex flex-col gap-2">
@@ -15,7 +26,10 @@ export default function PopOver({ children }: { children: React.ReactNode }) {
         <PopoverTrigger>{children}</PopoverTrigger>
         <PopoverContent>
           <div className="p-1 flex flex-col space-y-2">
-            <button className="flex items-center justify-start w-full p-2 rounded-lg transition-all duration-200 transform hover:bg-neutral-100 dark:hover:bg-neutral-800 hover:scale-105">
+            <button
+              className="flex items-center justify-start w-full p-2 rounded-lg transition-all duration-200 transform hover:bg-neutral-100 dark:hover:bg-neutral-800 hover:scale-105"
+              onClick={toggleUsers}
+            >
               <BiSolidUserPlus className="text-xl" />
               <span className="ml-2 flex-1 text-left">New Chat</span>
             </button>
