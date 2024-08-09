663750ecb9b91ddd3ff6024c37b1a89665f1640a
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-09 17:47:59 +0300

    Added chat resolver and chat type definitions

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,21 +1,26 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 import userResolvers from './resolvers/user.resolver';
+import chatResolvers from './resolvers/chat.resolver';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const userTypeDefs = readFileSync(path.join(__dirname, './typeDefs/user.graphql'), 'utf-8');
+const chatTypeDefs = readFileSync(path.join(__dirname, './typeDefs/chat.graphql'), 'utf-8');
 
 export const typeDefs = ` 
-  ${userTypeDefs}
+  ${userTypeDefs},
+  ${chatTypeDefs}
 `;
 
 export const resolvers = {
   Query: {
     ...userResolvers.Query,
+    ...chatResolvers.Query,
   },
   Mutation: {
     ...userResolvers.Mutation,
+    ...chatResolvers.Mutation,
   },
 };
