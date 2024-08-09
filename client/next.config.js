eff76768191a046fa476b2e5ef5faa25213b6e6e
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-09 17:35:32 +0300

    Whitelisted avatar API domain

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatar.iran.liara.run",
-        port: "",
-        pathname: "/username/*",
-      },
-    ],
+    domains: ["avatar.iran.liara.run"],
   },
 };
 
