0f54908cb05c82f69a3ff203716baefdfa372590
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-15 16:05:55 +0300

    Made use of the  previously implemented chat services

--- server/src/graphql/resolvers/chat.resolver.ts
+++ server/src/graphql/resolvers/chat.resolver.ts
@@ -1,5 +1,5 @@
 
-import { findSingleChat, createSingleChat, findGroupChat, createGroupChat, getUserChats } from '../services/chat.service';
+import { findSingleChat, createSingleChat, findGroupChat, createGroupChat, getUserChats, getMessagesByChatId, addMessage, getChatById } from '../services/chat.service';
 
 const chatResolvers = {
     Query: {
@@ -7,7 +7,18 @@ const chatResolvers = {
             const { id } = args;
             console.log('Chats query called with args:', args);
             return getUserChats(id)
-        }
+        },
+        getMessagesByChatId: async (_parent: any, args: any) => {
+          const { chatId } = args;
+          console.log('Getting the messages:', args);
+          return getMessagesByChatId(chatId);
+        },
+        getChatById: async (_parent: any, args: any) => {
+          const { id } = args;
+          console.log('Getting the chat:', args); 
+          return getChatById(id);
+      }
+      
     },
   Mutation: {
     createChat: async (_parent: any, args: any) => {
@@ -27,7 +38,26 @@ const chatResolvers = {
         return await createGroupChat(userIds);
       }
     },
+    addMessage: async (_parent: any, args: any, context: any) => {
+      const { chatId, content, imageUrl } = args;
+      const { user } = context;
+      console.log('Add message mutation called with args:', args);  
+      console.log('User:', user);
+      // if (!user) {
+      //   throw new Error("Not authenticated");
+      // }
+
+      // Call the service function to add the message
+      const newMessage = await addMessage({
+        chatId,
+        content,
+        imageUrl,
+        userId: user.id,
+      });
+
+      return newMessage;
   }
+}
 };
 
 export default chatResolvers;
