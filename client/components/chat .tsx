7c03b2784c7e332e25541c0400b549d4c258293f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-15 23:04:57 +0300

    Added date separators in chat and used useRef hook to scroll to the newly added messages

--- client/components/chat .tsx
+++ client/components/chat .tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useQuery, useMutation, gql } from "@apollo/client";
-import Image from "next/image";
 import { Chip, Spinner } from "@nextui-org/react";
 
 import ChatInput from "./chatInput";
@@ -65,6 +64,8 @@ function Chat({
   const [message, setMessage] = useState("");
   const [image, setImage] = useState<File | null>(null);
 
+  const messageContainerRef = useRef<HTMLDivElement>(null); // Ref for message container
+
   const { data: chatData } = useQuery(GET_CHAT_BY_ID, {
     variables: { id: chatId },
     skip: !chatId,
@@ -73,6 +74,7 @@ function Chat({
   const { data, loading, error } = useQuery(GET_MESSAGES_BY_CHAT_ID, {
     variables: { chatId },
     skip: !chatId,
+    pollInterval: 5000,
   });
 
   const [addMessage] = useMutation(ADD_MESSAGE, {
@@ -137,7 +139,7 @@ function Chat({
       hour12: true,
     });
 
-    return `${formattedDate} ${formattedTime}`;
+    return { formattedDate, formattedTime };
   };
 
   // Get the user's name that the current user is chatting with
@@ -145,76 +147,93 @@ function Chat({
     (user: any) => user.id !== currentUserId,
   );
 
+  // Scroll to the bottom of the message container whenever new messages are added
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
+    }
+  }, [data?.getMessagesByChatId]);
+
   if (loading)
     return (
-      <div className="hidden md:flex flex-col items-center justify-center md:w-[70%] w-full h-full bg-violet-700 dark:bg-neutral-950">
+      <div className="hidden md:flex flex-col items-center justify-center md:w-[65%] w-full h-full bg-violet-700 dark:bg-black">
         <Spinner color="white" />
       </div>
     );
   if (error) return <p>Error loading messages.</p>;
 
+  let lastMessageDate = ""; // Track the date of the last message to insert date separators
+
   return (
     <>
       {isChatVisible && (
-        <div className="absolute right-0 z-50 md:w-[70%] w-full h-full md:block dark:bg-black bg-violet-700 bg-gradient-to-br">
+        <div className="absolute right-0 z-50 md:w-[65%] w-full h-full md:block dark:bg-black bg-violet-700 bg-gradient-to-br">
           <ChatHeader
             chattingWithName={chattingWithUser?.name || null}
             handleBackClick={handleBackClick}
           />
-
-          {!chatId && (
-            <div className="flex items-center justify-center w-full h-full md:block">
-              <Image
-                alt={"logo"}
-                className="transition-transform transform duration-250 hover:-rotate-12"
-                height={150}
-                src="/logo.png"
-                width={150}
-              />
-            </div>
-          )}
           {chatId && (
             <div className="flex flex-col w-full h-screen space-y-4 ">
-              <div className="h-full p-4 mt-20 space-y-3 overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-neutral-200 scrollbar-track-violet-700 dark:scrollbar-track-neutral-950 dark:scrollbar-thumb-neutral-700">
-                {data?.getMessagesByChatId.map((msg: any) => (
-                  <div
-                    key={msg.id}
-                    className={`flex ${
-                      msg.sender.id === currentUserId
-                        ? "justify-end"
-                        : "justify-start"
-                    }`}
-                  >
-                    <Chip
-                      className={`py-1 px-[0.1rem] rounded-2xl max-w-xs h-full shadow-xl ${
-                        msg.sender.id === currentUserId
-                          ? "bg-violet-900 bg-gradient-to-r "
-                          : "dark:bg-neutral-900 bg-neutral-100 bg-gradient-to-l"
-                      }`}
-                    >
-                      <p
-                        className={`text-base text-start ${
-                          msg.sender.id === currentUserId
-                            ? " text-white "
-                            : " dark:text-white text-black"
-                        }`}
-                      >
-                        {msg.content}
-                      </p>
-                      <p
-                        className={`text-[0.6rem] text-end ${
-                          msg.sender.id === currentUserId
-                            ? "bg-gradient-to-r text-white "
-                            : " dark:text-white text-black"
-                        }`}
-                      >
-                        {formatDate(msg.createdAt)}
-                      </p>
-                    </Chip>
-                  </div>
-                ))}
-              </div>
+              <div
+                ref={messageContainerRef}
+                className="h-full p-4 mt-20 space-y-3 overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-neutral-200 scrollbar-track-violet-700 dark:scrollbar-track-neutral-950 dark:scrollbar-thumb-neutral-700"
+              >
+                {data?.getMessagesByChatId.map((msg: any) => {
+                  const { formattedDate, formattedTime } = formatDate(
+                    msg.createdAt,
+                  );
 
+                  const showDateSeparator = lastMessageDate !== formattedDate;
+
+                  lastMessageDate = formattedDate;
+
+                  return (
+                    <React.Fragment key={msg.id}>
+                      {showDateSeparator && (
+                        <div className="text-center text-sm text-gray-200 dark:text-gray-400 my-2">
+                          {formattedDate}
+                        </div>
+                      )}
+
+                      <div
+                        className={`flex ${
+                          msg.sender.id === currentUserId
+                            ? "justify-end"
+                            : "justify-start"
+                        }`}
+                      >
+                        <Chip
+                          className={`py-1 px-[0.1rem] rounded-2xl max-w-xs h-full shadow-xl ${
+                            msg.sender.id === currentUserId
+                              ? "bg-violet-900 bg-gradient-to-r "
+                              : "dark:bg-neutral-900 bg-neutral-100 bg-gradient-to-l"
+                          }`}
+                        >
+                          <p
+                            className={`text-base text-start ${
+                              msg.sender.id === currentUserId
+                                ? " text-white "
+                                : " dark:text-white text-black"
+                            }`}
+                          >
+                            {msg.content}
+                          </p>
+                          <p
+                            className={`text-[0.6rem] text-end ${
+                              msg.sender.id === currentUserId
+                                ? "bg-gradient-to-r text-white "
+                                : " dark:text-white text-black"
+                            }`}
+                          >
+                            {formattedTime}
+                          </p>
+                        </Chip>
+                      </div>
+                    </React.Fragment>
+                  );
+                })}
+              </div>
               <ChatInput
                 handleImageChange={handleImageChange}
                 handleSendMessage={handleSendMessage}
