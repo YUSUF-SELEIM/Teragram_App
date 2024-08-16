58cc7aac5a7c007ab8eea0547052bbf84492d67c
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-08-16 17:42:43 +0300

    Added image upload functionality to Chat component
    Prevented layout breaks by long messages by adding "break-all" tailwind class

--- client/components/chat .tsx
+++ client/components/chat .tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useQuery, useMutation, gql } from "@apollo/client";
 import { Chip, Spinner } from "@nextui-org/react";
+import { CldImage } from "next-cloudinary";
 
 import ChatInput from "./chatInput";
 import ChatHeader from "./chatHeader";
@@ -21,6 +22,7 @@ const GET_MESSAGES_BY_CHAT_ID = gql`
     getMessagesByChatId(chatId: $chatId) {
       id
       content
+      imageUrl
       createdAt
       sender {
         id
@@ -31,12 +33,13 @@ const GET_MESSAGES_BY_CHAT_ID = gql`
 `;
 
 const ADD_MESSAGE = gql`
-  mutation AddMessage($chatId: ID!, $content: String!) {
-    addMessage(chatId: $chatId, content: $content) {
+  mutation AddMessage($chatId: ID!, $content: String!, $imageUrl: String) {
+    addMessage(chatId: $chatId, content: $content, imageUrl: $imageUrl) {
       sender {
         email
       }
       content
+      imageUrl
       createdAt
       id
       chat {
@@ -62,7 +65,8 @@ function Chat({
   isChatVisible: boolean;
 }) {
   const [message, setMessage] = useState("");
-  const [image, setImage] = useState<File | null>(null);
+  const [image, setImage] = useState<String | null>(null);
+  const [isUploading, setIsUploading] = useState(false);
 
   const messageContainerRef = useRef<HTMLDivElement>(null); // Ref for message container
 
@@ -104,23 +108,50 @@ function Chat({
     },
   });
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (!message && !image) return;
 
     addMessage({
       variables: {
         chatId,
         content: message,
+        imageUrl: image,
       },
     });
-
     setMessage("");
     setImage(null);
   };
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageChange = async (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     if (event.target.files && event.target.files.length > 0) {
-      setImage(event.target.files[0]);
+      const file = event.target.files[0];
+      const formData = new FormData();
+
+      setIsUploading(true);
+
+      formData.append("file", file);
+      formData.append("upload_preset", "Teragram");
+
+      try {
+        const response = await fetch(
+          `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+          {
+            method: "POST",
+            body: formData,
+          },
+        );
+
+        const data = await response.json();
+        const imageUrl = data.secure_url;
+
+        setImage(imageUrl);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      } finally {
+        setIsUploading(false);
+      }
     }
   };
 
@@ -177,7 +208,7 @@ function Chat({
             <div className="flex flex-col w-full h-screen space-y-4 ">
               <div
                 ref={messageContainerRef}
-                className="h-full p-4 mt-20 space-y-3 overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-neutral-200 scrollbar-track-violet-700 dark:scrollbar-track-neutral-950 dark:scrollbar-thumb-neutral-700"
+                className="h-full p-4 mt-20 space-y-3 overflow-x-hidden overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-neutral-200 scrollbar-track-violet-700 dark:scrollbar-track-neutral-950 dark:scrollbar-thumb-neutral-700"
               >
                 {data?.getMessagesByChatId.map((msg: any) => {
                   const { formattedDate, formattedTime } = formatDate(
@@ -191,7 +222,7 @@ function Chat({
                   return (
                     <React.Fragment key={msg.id}>
                       {showDateSeparator && (
-                        <div className="text-center text-sm text-gray-200 dark:text-gray-400 my-2">
+                        <div className="my-2 text-sm text-center text-gray-200 dark:text-gray-400">
                           {formattedDate}
                         </div>
                       )}
@@ -204,21 +235,32 @@ function Chat({
                         }`}
                       >
                         <Chip
-                          className={`py-1 px-[0.1rem] rounded-2xl max-w-xs h-full shadow-xl ${
+                          className={`py-1 px-[0.1rem] rounded-2xl h-full max-w-xs w-fit shadow-xl ${
                             msg.sender.id === currentUserId
                               ? "bg-violet-900 bg-gradient-to-r "
                               : "dark:bg-neutral-900 bg-neutral-100 bg-gradient-to-l"
                           }`}
                         >
-                          <p
-                            className={`text-base text-start ${
-                              msg.sender.id === currentUserId
-                                ? " text-white "
-                                : " dark:text-white text-black"
-                            }`}
-                          >
-                            {msg.content}
-                          </p>
+                          {msg.imageUrl ? (
+                            <CldImage
+                              alt="Uploaded image"
+                              className="my-2 rounded-2xl"
+                              crop="scale"
+                              height="200"
+                              src={msg.imageUrl}
+                              width="200"
+                            />
+                          ) : (
+                            <p
+                              className={`text-base text-start break-all whitespace-normal text-wrap ${
+                                msg.sender.id === currentUserId
+                                  ? " text-white "
+                                  : " dark:text-white text-black"
+                              }`}
+                            >
+                              {msg.content}
+                            </p>
+                          )}
                           <p
                             className={`text-[0.6rem] text-end ${
                               msg.sender.id === currentUserId
@@ -238,6 +280,7 @@ function Chat({
                 handleImageChange={handleImageChange}
                 handleSendMessage={handleSendMessage}
                 image={image}
+                isUploading={isUploading}
                 message={message}
                 setMessage={setMessage}
               />
